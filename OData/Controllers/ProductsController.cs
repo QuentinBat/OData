e4d983b3fed9d78378d96376749beae479766d16
BATON Quentin <213558@supinfo.com>
2019-12-13 20:53:36 +0100

    Ajoutez des fichiers projet.

---
+++ OData/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OData.Models;
+
+namespace OData.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ODataController
+    {
+        // Initialisation in memory
+        private List<Product> products = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                Name = "Bread",
+            },
+            new Product()
+            {
+                Id = 1,
+                Name = "Milk",
+            },
+            new Product()
+            {
+                Id = 1,
+                Name = "Potatoes",
+            }
+        };
+
+        [EnableQuery(PageSize = 2)] // Annotation OData
+        public List<Product> Get()
+        {
+            return products;
+        }
+    }
+}
---
+++ OData/Startup.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OData.Models;
+
+namespace OData
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddOData();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            var builder = new ODataConventionModelBuilder(app.ApplicationServices);
+
+            builder.EntitySet<Product>("Products");
+
+            app.UseMvc(routeBuilder =>
+            {
+                routeBuilder.EnableDependencyInjection();
+
+                // and this line to enable OData query option, for example $filter
+                routeBuilder.Select().Expand().Filter().OrderBy().Count().MaxTop(null);
+
+                routeBuilder.MapODataServiceRoute("ODataRoute", "odata", builder.GetEdmModel());
+            });
+        }
+    }
+}
